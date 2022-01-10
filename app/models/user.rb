7a3f065f10a7154589816abd745ded40566a065f
fumtas1k <fumta01a7@gmail.com>
2022-01-11 00:24:34 +0900

    add:データベース、テーブル、モデル作成

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :text             not null
+#  name            :string           not null
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+end
