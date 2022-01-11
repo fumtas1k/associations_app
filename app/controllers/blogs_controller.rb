fa80c77e359e0d3671611e6c961903a5f900dc3f
fumtas1k <fumta01a7@gmail.com>
2022-01-11 10:34:12 +0900

    add:user session blogのコントローラを作成

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,16 @@
+class BlogsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def confirm
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'blogs/new'
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/edit'
+  get 'blogs/confirm'
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/confirm'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
